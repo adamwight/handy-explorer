6e53246fa58a8e3ef232914208e13adaaa904ce3
Adam Wight <adam.wight@wikimedia.de>
2019-10-16 23:17:47 +0200

    Put chartData in the right place

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,25 +23,21 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class Home extends Vue {
     private param1: number = 0;
 
-    @Prop() private chartData: object = this.calculateChartData();
-
     private handleParam1(value: number) {
         this.param1 = value;
-        this.chartData = this.calculateChartData();
+        // TODO force update?
     }
 
-    private calculateChartData(): object {
+    get chartData() {
         return {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
                 {
                     label: 'Data One',
-                    backgroundColor: '#FC2525',
-                    data: [40, 39, 10, 40, 39, 80, 40].map( (val) => val + this.param1 ),
+                    data: [40, 39, 10, 40, 39, 80, 40].map((val) => val + this.param1),
                 },
                 {
                     label: 'Data Two',
-                    backgroundColor: '#05CBE1',
                     data: [60, 55, 32, 10, 2, 12, 53],
                 },
             ],
