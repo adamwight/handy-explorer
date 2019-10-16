34269e6bdaab422f38c05ce610d9c02deccab816
Adam Wight <adam.wight@wikimedia.de>
2019-10-16 10:52:29 +0200

    Wiring is correct.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <ParameterControls @param1Change="handleParam1" />
-    <LineChart :chart-data="displaydatacollection" :options="chartoptions" />
+    <LineChart :chart-data="chartData" :options="chartoptions" />
   </div>
 </template>
 <script lang="ts">
@@ -12,24 +12,6 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component({
     components: {LineChart, ParameterControls},
     computed: {
-        displaydatacollection() {
-            return {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [
-                    {
-                        label: 'Data One',
-                        backgroundColor: '#FC2525',
-                        data: [40, 39, 10, 40, 39, 80, 40],
-                    },
-                    {
-                        label: 'Data Two',
-                        backgroundColor: '#05CBE1',
-                        data: [60, 55, 32, 10, 2, 12, 53],
-                    },
-                ],
-            };
-        },
-
         chartoptions() {
             return {
                 responsive: true,
@@ -41,9 +23,29 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class Home extends Vue {
     private param1: number = 0;
 
-    handleParam1(value: number) {
-        console.log(value);
+    @Prop() private chartData: object = this.calculateChartData();
+
+    private handleParam1(value: number) {
         this.param1 = value;
+        this.chartData = this.calculateChartData();
     }
-};
+
+    private calculateChartData(): object {
+        return {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'Data One',
+                    backgroundColor: '#FC2525',
+                    data: [40, 39, 10, 40, 39, 80, 40].map( (val) => val + this.param1 ),
+                },
+                {
+                    label: 'Data Two',
+                    backgroundColor: '#05CBE1',
+                    data: [60, 55, 32, 10, 2, 12, 53],
+                },
+            ],
+        };
+    }
+}
 </script>
