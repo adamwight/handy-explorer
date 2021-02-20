ef32743bcbca41b7f9bab069e9cf279be53525cb
Adam Wight <adam.wight@wikimedia.de>
2021-02-20 23:54:29 +0100

    Remove font customization

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@ body {
   height: 100vh;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
